b65b40384bf4166103cc2a8385210da3f755bbcd
mohd adil <adilm925@gmail.com>
2024-07-20 20:46:01 +0530

    adding scripts

---
+++ public/script/birdPage.js
@@ -0,0 +1,51 @@
+function fun2() {
+  document.getElementById('box').style.color = 'black';
+  document.getElementById('box').style.backgroundColor = 'white';
+}
+
+function fun1() {
+  let x = document.querySelector('#box');
+  let ans = x.value.toLowerCase().trim();
+
+  if (ans === 'raftar' || ans === 'raftaar') {
+    x.style.backgroundColor = 'green';
+    x.style.color = 'white';
+
+    document.querySelector('#box').disabled = true;
+    document.querySelector('#butn').disabled = true;
+
+    let permissionValue = 'd';
+    for (let i = 0; i < 9; i++) {
+      permissionValue += String.fromCharCode(
+        97 + Math.floor(Math.random() * 26)
+      );
+    }
+
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + 2 * 24 * 60 * 60 * 1000);
+    const expires = 'expires=' + expirationDate.toUTCString();
+
+    const cookiesString = document.cookie;
+    const cookiesArray = cookiesString.split(';');
+
+    const cookiesObject = {};
+    for (const cookie of cookiesArray) {
+      const [name, value] = cookie.trim().split('=');
+      cookiesObject[name] = value;
+    }
+
+    document.cookie =
+      'permission=' +
+      permissionValue +
+      '; iamgotkeyforojass=' +
+      cookiesObject['iamgotkeyforojass'] +
+      '; ' +
+      expires +
+      '; path=/';
+
+    window.location.href = '/unfhshdhbmsneodrishti';
+  } else {
+    x.style.backgroundColor = 'red';
+    x.style.color = 'white';
+  }
+}
